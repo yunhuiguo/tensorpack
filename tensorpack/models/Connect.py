8bd4634f7b77cf012cf16f2600cefa0af9841fe6
henry <gyhui@zju.edu.cn>
2018-02-22 20:37:15 -0800

    add connection layer

--- tensorpack/models/Connect.py
+++ tensorpack/models/Connect.py
@@ -68,36 +68,3 @@ class Connect(object):
         """
         print(self._sensor_list)
         return self
-
-
-
-
-
-
-
-
-
-
-
-
-
-@layer_register(log_shape=True)
-def Connect(sensors = [], method = "inner_product"):
-    def model(x = [], is_training = True):
-        outputs = []
-
-        for sensor_idx, sensor in enumerate(sensors):
-            with tf.variable_scope("connect_sensor_" + str(sensor_idx)):
-                sensor_output = sensor(x[sensor_idx])
-                n_input = int(sensor_output.shape[1])
-                if method == "inner_product":
-                    w = tf.get_variable("weight_"+str(sensor_idx), [n_input, 1],
-                        initializer=tf.contrib.layers.xavier_initializer())
-
-                    output = tf.matmul(sensor_output, w)
-                    outputs.append(output)
-                elif method == "concat":
-                    outputs.append(sensor_output)
-        outputs = tf.concat(outputs, axis=1)
-        return outputs
-    return model
