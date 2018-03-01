b74d05bd5efd4bac6ed9c89a6008cc8bab293a33
henry <gyhui@zju.edu.cn>
2018-02-28 16:50:31 -0800

    changes

--- tensorpack/callbacks/save_sensor_networks.py
+++ tensorpack/callbacks/save_sensor_networks.py
@@ -11,36 +11,16 @@ from ..tfutils.common import get_tf_version_number
 
 __all__ = ['SaveSensorNetworks']
 
-
-def freeze_graph(sess):
+def freeze_graph(sess, var_list):
     # convert_variables_to_constants(sess, input_graph_def, output_node_names, variable_names_whitelist=None)
-    with gfile.FastGFile("./tmp/" + "graph.pb", 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
+    #with gfile.FastGFile("./tmp/" + "graph.pb", 'rb') as f:
+    #    graph_def = tf.GraphDef()
+    #    graph_def.ParseFromString(f.read())
 
-    frozen_graph_def = convert_variables_to_constants(sess, graph_def, ["connect_sensor_0/hand_1/hand_output"])
-
-    with tf.gfile.GFile("./tmp/" + "frozen.pb", "wb") as f:
-        f.write(frozen_graph_def.SerializeToString())
-
-    return frozen_graph_def
-           
-
-saver.save(sess, "./tmp/model", write_meta_graph=True, global_step=1)
-with open("./tmp/" + "graph.pb", 'wb') as f:
-    f.write(sess.graph_def.SerializeToString())
-
-freeze_graph(sess)
-
-  g = tf.Graph()
-    with g.as_default():
-
-
-for v in g.as_graph_def().node:
-    print v.name
-
-saver = tf.train.Saver()
-
+    for var in var_list:
+        frozen_graph_def = convert_variables_to_constants(sess, sess.graph_def, ["connect_sensor_0/hand_1/output"])
+        with tf.gfile.GFile("./tmp/" + var + "frozen.pb", "wb") as f:
+            f.write(frozen_graph_def.SerializeToString())
 
 class SaveSensorNetworks(Callback):
     """
@@ -60,14 +40,9 @@ class SaveSensorNetworks(Callback):
             var_collections (str or list of str): collection of the variables (or list of collections) to save.
         """
         self._sess = tf.get_default_session()
-        self._prefix = ""
-
-        
-
-        self.var_collections 
-        
-        self._max_to_keep = max_to_keep
-        self._keep_every_n_hours = keep_checkpoint_every_n_hours
+        self._graph = tf.get_default_graph()
+        self._prefix = "InferenceTower/"
+        self._var_list = []
 
         if saving_dir is None:
             saving_dir = logger.get_logger_dir()
@@ -81,44 +56,20 @@ class SaveSensorNetworks(Callback):
     def _setup_graph(self):
         assert self.saving_dir is not None, \
             "ModelSaver() doesn't have a valid checkpoint directory."
-        vars = []
-        for key in self.var_collections:
-            vars.extend(tf.get_collection(key))
-        vars = list(set(vars))
-        self.path = os.path.join(self.saving_dir, 'model')
-        if get_tf_version_number() <= 1.1:
 
-            self.saver = tf.train.Saver(
-                var_list=vars,
-                max_to_keep=self._max_to_keep,
-                keep_checkpoint_every_n_hours=self._keep_every_n_hours,
-                write_version=tf.train.SaverDef.V2)
-        else:
-            self.saver = tf.train.Saver(
-                var_list=vars,
-                max_to_keep=self._max_to_keep,
-                keep_checkpoint_every_n_hours=self._keep_every_n_hours,
-                write_version=tf.train.SaverDef.V2,
-                save_relative_paths=True)
-        # Scaffold will call saver.build from this collection
-        tf.add_to_collection(tf.GraphKeys.SAVERS, self.saver)
+        for sensor in SensorsToSave:
+            self._var_list.append(self._prefix + sensor + "_output")
+
+        self.path = os.path.join(self.saving_dir, 'model')
 
     def _before_train(self):
         # graph is finalized, OK to write it now.
-        time = datetime.now().strftime('%m%d-%H%M%S')
-        self.saver.export_meta_graph(
-            os.path.join(self.saving_dir,
-                         'graph-{}.meta'.format(time)),
-            collection_list=self.graph.get_all_collection_keys())
-
+        #time = datetime.now().strftime('%m%d-%H%M%S')
+        pass 
 
     def _trigger(self):
-        try:
-            self.saver.save(
-                tf.get_default_session(),
-                self.path,
-                global_step=tf.train.get_global_step(),
-                write_meta_graph=False)
+        try:   
+            freeze_graph(self._sess, self._var_list)
             logger.info("Model saved to %s." % tf.train.get_checkpoint_state(self.saving_dir).model_checkpoint_path)
         except (OSError, IOError, tf.errors.PermissionDeniedError,
                 tf.errors.ResourceExhaustedError):   # disk error sometimes.. just ignore it
