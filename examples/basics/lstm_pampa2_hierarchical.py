0b703d702d678d8f95cf0ef6701137be225866a7
henry <gyhui@zju.edu.cn>
2018-03-13 21:19:12 -0700

    changes

--- examples/basics/lstm_pampa2_hierarchical.py
+++ examples/basics/lstm_pampa2_hierarchical.py
@@ -50,7 +50,7 @@ def main(subject, train_data, one_hot_train_labels, \
                             validation_data, one_hot_validation_labels, \
                             l2_regularizer, \
                             keepprob, \
-                            learning_rate):
+                            learning_rate, output_num):
 
     tf.reset_default_graph()	
     train_accuracy_batches = []
@@ -65,7 +65,7 @@ def main(subject, train_data, one_hot_train_labels, \
     n_hidden = 64  
     n_classes = 5
     n_steps = 25
-    output_num = 5
+    #output_num = 5
 
     '''
     heart = img[:,0].reshape((-1,1))
@@ -279,86 +279,89 @@ if __name__ == '__main__':
     l2_regularizers = [0.01, 0.1, 1]
     keep_probs = [0.5]
     learning_rates = [0.0001, 0.001, 0.01, 0.1]
+    output_nums = [1,,3,,5, 7, 9]
 
     for learning_rate in learning_rates:
         for l2 in l2_regularizers:
             for keep_prob in keep_probs:
+                for output_num in output_nums:
 
-                f = open("./summary_fully_lstm.txt", "a+")
+                    f = open("./summary_fully_lstm.txt", "a+")
 
-                f.write("l2 = " + str(l2) \
-                    + " " + "keep_prob = " + str(keep_prob) \
-                    + " " + "learning rate = " + str(learning_rate) \
-                    + "\n")     
+                    f.write("l2 = " + str(l2) \
+                        + " " + "keep_prob = " + str(keep_prob) \
+                        + " " + "learning rate = " + str(learning_rate) \
+                        + " " + "output_num = " + str(output_num) \
+                        + "\n")     
 
-                f.write("[")        
-                f.close()
+                    f.write("[")        
+                    f.close()
 
-                valiation_time = 7
-                for i in range(valiation_time):
-                    
-                    test_data = data_list[i]
-                    test_labels = label_list[i]
+                    valiation_time = 7
+                    for i in range(valiation_time):
+                        
+                        test_data = data_list[i]
+                        test_labels = label_list[i]
 
-                    valiation_idx = random.randint(0, valiation_time-1)
-                    while valiation_idx == i:
-                         valiation_idx = random.randint(0, valiation_time-1)
+                        valiation_idx = random.randint(0, valiation_time-1)
+                        while valiation_idx == i:
+                             valiation_idx = random.randint(0, valiation_time-1)
 
-                    validation_data = data_list[valiation_idx]
-                    validation_labels = label_list[valiation_idx]
+                        validation_data = data_list[valiation_idx]
+                        validation_labels = label_list[valiation_idx]
 
-                    train_data_list = data_list[:]
-                    train_label_list = label_list[:]
+                        train_data_list = data_list[:]
+                        train_label_list = label_list[:]
 
-                    train_data_list.remove(test_data)
-                    train_data_list.remove(validation_data)
-                    train_label_list.remove(test_labels)
-                    train_label_list.remove(validation_labels)
-
-
-                    train_data = np.concatenate(train_data_list)
-                    train_labels = np.concatenate(train_label_list)
+                        train_data_list.remove(test_data)
+                        train_data_list.remove(validation_data)
+                        train_label_list.remove(test_labels)
+                        train_label_list.remove(validation_labels)
 
 
-                    train_labels =  train_labels.reshape(train_labels.shape[0],1)
-                    validation_labels = validation_labels.reshape(validation_labels.shape[0],1)
-                    test_labels = test_labels.reshape(test_labels.shape[0],1)
+                        train_data = np.concatenate(train_data_list)
+                        train_labels = np.concatenate(train_label_list)
 
 
-                    idx1 = np.nonzero(train_labels != 99)
-                    train_data = train_data[idx1[0]]
-                    train_labels = train_labels[idx1[0]]
-
-                    idx2 = np.nonzero(test_labels != 99)
-                    test_data = test_data[idx2[0]]
-                    test_labels = test_labels[idx2[0]]
+                        train_labels =  train_labels.reshape(train_labels.shape[0],1)
+                        validation_labels = validation_labels.reshape(validation_labels.shape[0],1)
+                        test_labels = test_labels.reshape(test_labels.shape[0],1)
 
 
-                    idx3 = np.nonzero(validation_labels != 99)
-                    validation_data = validation_data[idx3[0]]
-                    validation_labels = validation_labels[idx3[0]]
+                        idx1 = np.nonzero(train_labels != 99)
+                        train_data = train_data[idx1[0]]
+                        train_labels = train_labels[idx1[0]]
+
+                        idx2 = np.nonzero(test_labels != 99)
+                        test_data = test_data[idx2[0]]
+                        test_labels = test_labels[idx2[0]]
 
 
-                    train_data, train_labels = segment_pa2( train_data,train_labels, input_width)
-                    test_data, test_labels = segment_pa2(test_data,test_labels, input_width)
-                    validation_data, validation_labels = segment_pa2(validation_data, validation_labels, input_width)
-
-                    train_labels = train_labels.astype(int)
-                    test_labels = test_labels.astype(int)
-                    validation_labels = validation_labels.astype(int)
+                        idx3 = np.nonzero(validation_labels != 99)
+                        validation_data = validation_data[idx3[0]]
+                        validation_labels = validation_labels[idx3[0]]
 
 
-                    one_hot_train_labels = np.eye(5)[train_labels].reshape(train_labels.shape[0], 5)
-                    one_hot_test_labels = np.eye(5)[test_labels].reshape(test_labels.shape[0], 5)
-                    one_hot_validation_labels = np.eye(5)[validation_labels].reshape(validation_labels.shape[0], 5)
+                        train_data, train_labels = segment_pa2( train_data,train_labels, input_width)
+                        test_data, test_labels = segment_pa2(test_data,test_labels, input_width)
+                        validation_data, validation_labels = segment_pa2(validation_data, validation_labels, input_width)
 
-    
-                    main(i, train_data, one_hot_train_labels, \
-                        test_data, one_hot_test_labels, \
-                        validation_data, one_hot_validation_labels, \
-                        l2, \
-                        keep_prob, \
-                        learning_rate)
+                        train_labels = train_labels.astype(int)
+                        test_labels = test_labels.astype(int)
+                        validation_labels = validation_labels.astype(int)
 
 
-    
+                        one_hot_train_labels = np.eye(5)[train_labels].reshape(train_labels.shape[0], 5)
+                        one_hot_test_labels = np.eye(5)[test_labels].reshape(test_labels.shape[0], 5)
+                        one_hot_validation_labels = np.eye(5)[validation_labels].reshape(validation_labels.shape[0], 5)
+
+        
+                        main(i, train_data, one_hot_train_labels, \
+                            test_data, one_hot_test_labels, \
+                            validation_data, one_hot_validation_labels, \
+                            l2, \
+                            keep_prob, \
+                            learning_rate, output_num)
+
+
+        
